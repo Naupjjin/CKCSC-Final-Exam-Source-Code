17fde9b235ae47538e92c7eaa8d52931c3abd819
Naup <naup96721@gmail.com>
2023-12-23 16:28:13 +0800

    jhdj

--- Web/hard/app.py
+++ Web/hard/app.py
@@ -1,10 +1,16 @@
 import sqlite3
 from flask import *
 import json
+from flask_session import *
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SESSION_TYPE'] = 'filesystem'
+
 
 @app.route("/")
 def home1():
+    session['level']=1
     return render_template("home1.html")
 
 @app.route("/login", methods=["GET", "POST"])
@@ -25,13 +31,16 @@ def login():
 
     conn.close()
     if user:
-        return redirect('oeajifpojaqeiowrjiwoejwiojrieowjri')
+        session['level']=2
+        return redirect('level2')
         
     else:
         return render_template('get_flag.html')
     
-@app.route("/oeajifpojaqeiowrjiwoejwiojrieowjri")
-def oeajifpojaqeiowrjiwoejwiojrieowjri():
+@app.route("/level2")
+def level2():
+    if session['level']==1:
+        return redirect('/')
     return render_template("home2.html")
 
 @app.route("/login2", methods=["GET", "POST"])
@@ -56,13 +65,18 @@ def login2():
 
     conn.close()
     if user:
-        return redirect('foshdfiaopreiaowjfpoaweituripowa')
+        session['level']=3
+        return redirect('level3')
         
     else:
         return render_template('lose.html')
 
-@app.route("/foshdfiaopreiaowjfpoaweituripowa")
-def foshdfiaopreiaowjfpoaweituripowa():
+@app.route("/level3")
+def level3():
+    if session['level']==1:
+        return redirect('/')
+    if session['level']==2:
+        return redirect('level2')
     return render_template("home3.html")
 
 @app.route("/login3", methods=["GET", "POST"])
